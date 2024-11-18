a59edcc84705e5a359a2de52e0677ee43ed97b89
Seu Nome <seuemail@exemplo.com>
2024-11-18 18:42:33 -0300

    Corrigir configurações para deploy no Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,132 @@
-import Image from "next/image";
+'use client'; // Adiciona essa linha para marcar o arquivo como cliente
 
-export default function Home() {
+import React, { useEffect } from 'react';
+import { FaShoppingCart, FaSearch, FaRegHandshake, FaStore, FaPhoneAlt, FaBlog, FaWhatsapp } from 'react-icons/fa';
+
+const Home = () => {
+  useEffect(() => {
+    const heading = document.querySelector('.heading');
+    if (heading) {
+      setTimeout(() => {
+        heading.classList.add('visible');
+      }, 500);
+    }
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="w-full h-screen md:h-auto flex flex-col md:flex-row overflow-hidden md:overflow-visible">
+      {/* Lado esquerdo - fundo roxo escuro, alinhado à esquerda */}
+      <div className="flex flex-col justify-center p-8 bg-[#3D113A] text-white w-full md:w-1/2 h-full md:h-screen relative">
+        <img
+          src="/logo.PNG"
+          alt="Logo"
+          className="
+          w-[138px] h-auto mt-0.5 ml-0 mb-0 
+          md:w-[127px] md:mt-7 md:ml-4 md:mb-4 
+          md:absolute md:top-1 md:left-0
+        "
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="text-left " >
+          {/* Título em uma linha acima */}
+          <h2 className="text-[25px] font-semibold leading-[28px] text-white bg-[#5D1B52] rounded-md p-2 inline-block mt-10 mb-4">
+            Mtrek: Economize, <br />
+            Ganhe e Realize.
+          </h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {/* Texto descritivo logo abaixo */}
+          <p className="text-[15px] font-light leading-[18px] text-white max-w-[80%]">
+            Tudo em um só lugar! Conheça a maior Plataforma Brasileira que transforma suas Compras Online, Conta de Energia e Mercado Local em ganhos. Torne-se membro e comece a fazer a diferença no seu bolso agora mesmo!
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 bg-white text-gray-800 rounded-full p-3 shadow-lg cursor-pointer">
+          <span>&gt;</span>
+        </div>
+      </div>
+
+      {/* Lado direito - fundo cinza claro cobrindo o lado direito */}
+      <div className="flex justify-center items-center bg-[#F7F7F7] w-full md:w-1/2 h-full md:h-screen">
+        {/* Contêiner branco centralizado com borda suave e sombra */}
+        <div
+          className="flex flex-col justify-center items-center bg-white shadow-lg p-6 w-full max-w-md md:max-w-[350px] h-auto md:h-[500px] rounded-lg box-border"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {/* Título e subtítulo acima do menu */}
+          <div className="text-left mb-4 w-full ">
+            <h1 className="text-[24px] font-semibold leading-[23px] text-gray-900 ">
+              Economize Mais <br />
+              com a Mtrek.
+            </h1>
+            <p className="mt-2 text-[15px] font-light leading-[16px] text-gray-700">
+              Todos os caminhos para <span className="font-semibold">Economizar</span><br /> e realizar estão aqui! Explore e descubra como começar agora!
+            </p>
+          </div>
+
+          {/* Menu e botões com ícones centralizados */}
+          <div className="flex flex-col gap-2 w-full max-w-[250px] mb-2">
+            {[
+              {
+                text: 'Quero ser Membro',
+                icon: <FaStore className="text-sm" />,
+                bgColor: 'bg-[#5D1B52]',
+                textColor: 'text-white',
+                link: '/membro'
+              },
+              {
+                text: 'Loja Online',
+                icon: <FaShoppingCart className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/loja'
+              },
+              {
+                text: 'Mercado Livre de Energia',
+                icon: <FaSearch className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/energia'
+              },
+              {
+                text: 'Mercado Local',
+                icon: <FaRegHandshake className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/mercado-local'
+              },
+              {
+                text: 'Blog Mtrek',
+                icon: <FaBlog className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/blog'
+              },
+              {
+                text: 'Central de Ajuda',
+                icon: <FaPhoneAlt className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/ajuda'
+              },
+              {
+                text: 'WhatsApp',
+                icon: <FaWhatsapp className="text-sm" />,
+                bgColor: 'bg-transparent',
+                textColor: 'text-gray-800 border-gray-300 border',
+                link: '/whatsapp'
+              }
+            ].map((item, index) => (
+              <a
+                key={index}
+                href={item.link}
+                className={`flex items-center justify-start gap-2 py-1 px-3 ${item.bgColor} ${item.textColor} rounded-[8px] text-[14px] font-medium transition duration-300 ease-in-out hover:bg-[#5D1B52] hover:text-white`} // Efeito hover roxo
+              >
+                <span>{item.icon}</span>
+                <span>{item.text}</span>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
